3baca3acd0449ea665416757756e94be4c76fb8e
Letizialf <letilf2005@gmail.com>
2024-11-15 12:52:59 +0100

    Engadidas máis funcionalidades

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Blog
 # Create your views here.
 
 def all_blogs(request):
-    blog = Blog.objects.all()
+    blog = Blog.objects.order_by('-date')[:3] 
+    # Para que só se mostre un número determinado de entradas --> 
+    #   [: numeroEntradasVisibles]
+    # Para que a orde sexa descendente engadimos un '-' antes do campo
     return render(request, 'blog/all_blogs.html', {'Blog':blog})
+
+def detail(request, blog_id):
+    blog = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'blog/detail.html', {'id':blog_id})
