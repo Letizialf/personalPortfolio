20fd09d38e07ffe52e91942b5542a0ef598f68f8
Letizialf <letilf2005@gmail.com>
2024-11-15 11:43:46 +0100

    Posto bonito

--- blog/models.py
+++ blog/models.py
@@ -3,5 +3,5 @@ from django.db import models
 # Create your models here.
 class Blog(models.Model):
     title = models.CharField(max_length=200)
-    description = models.CharField(max_length=250)
+    description = models.TextField()
     date = models.DateField(auto_now=True)
