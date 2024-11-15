f8d9c9bb8db61b9491f3700838182145f543b5f0
Letizialf <letilf2005@gmail.com>
2024-11-15 11:35:39 +0100

    Engadida a data

--- blog/models.py
+++ blog/models.py
@@ -4,4 +4,4 @@ from django.db import models
 class Blog(models.Model):
     title = models.CharField(max_length=200)
     description = models.CharField(max_length=250)
-    date = models.DateField()
+    date = models.DateField(auto_now=True)
