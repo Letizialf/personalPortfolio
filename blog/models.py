ff9aee0c2be9496c64f4673ff9c1f3b360fe11e7
Letizialf <letilf2005@gmail.com>
2024-11-20 10:48:02 +0100

    Proxecto actualizado e finalizado

--- blog/models.py
+++ blog/models.py
@@ -5,6 +5,7 @@ class Blog(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     date = models.DateField(auto_now=True)
+    image = models.ImageField(upload_to='portfolio/images', blank=True)
 
     def __str__(self):
         return self.title
